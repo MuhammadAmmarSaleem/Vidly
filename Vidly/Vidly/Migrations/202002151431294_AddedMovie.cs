70dd6d9d1491659afa2f6e60bdd2cdbe6d4ba05e
Ammar Saleem <ammarsaleem1995@gmail.com>
2020-02-15 19:36:42 +0500

    added CustomerList.cshtml, MovieList.cshtml, Movie Model.

---
+++ Vidly/Vidly/Migrations/202002151431294_AddedMovie.cs
@@ -0,0 +1,26 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedMovie : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
